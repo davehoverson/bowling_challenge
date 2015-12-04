80f03b503c3258939e70b4e59194c5f5c98ffe85
tongtonk <davidhoverson@yahoo.com>
2015-12-04 16:25:40 -0600

    Decided to end coding at 4:30pm Dec 4

--- script.js
+++ script.js
@@ -2,15 +2,24 @@ var app = angular.module('bowling', []);
 
 
 app.controller('MainController', ['$scope', function($scope){
-
-	var frameNumber = 0;
+	//working on hiding the 3rd ball - ran out of time.
+	// $scope.ballThree = !$scope.ballThree;
+	
+	$scope.frameNumber = 0;
+	//working on hiding the 3rd ball - ran out of time.
+	// if ($scope.frameNumber = 9){
+	// 	!$scope.ballThree = $scope.ballThree;
+ //    };
 
 	$scope.scores = [];
-	     
+
+	//still working on excluding or hiding strings from the calculation during game.
+	$scope.total = 0; 
+
 	$scope.enterScore = function(a,b,c){
-		var i = frameNumber;
-		if (frameNumber == 9){
-			frameTen(a,b,c);
+		var i = $scope.frameNumber;
+		if ($scope.frameNumber == 9){
+			$scope.frameTen(a,b,c);
 		} else if (a == 10 && $scope.scores[i-2] == 'X' && $scope.scores[i-1] == 'X'){
 			$scope.scores[i-2] = 30;
 			$scope.scores.push('X');
@@ -45,36 +54,46 @@ app.controller('MainController', ['$scope', function($scope){
 			$scope.scores.push(a+b);
 		} else {
 			alert("Total must be 10 or less!");
-			frameNumber--;
+			$scope.frameNumber--;
 		}
-		frameNumber++;
-	};
-
-	$scope.frameTen = function(a,b,c){
-		var i = 9;
-		if (a == 10 && $scope.scores[i-2] == 'X' && $scope.scores[i-1] == 'X'){
-			$scope.scores[i-2] = 30;
-			$scope.scores[i-1]= 10 + (a+b);
-			$scope.scores.push(a+b+c);
-		} else if (a == 10 && $scope.scores[i-1] == 'X'){
-			$scope.scores[i-1] = 10 + (a+b);
-			$scope.scores.push(a+b+c);
-		} else if ($scope.scores[i-1] == '/'){
-			$scope.scores[i-1] = 10 + a;
-			$scope.scores.push(a+b)+c;
-		} else {
-			$scope.scores.push(a+b)+c;
+		$scope.frameNumber++;
+		$scope.total = $scope.scores.reduce(function (a,b) {
+	  		return a+b;
+	  	}, 0);
+	  		//have not tested and resolved all conditional aspects of frameTen.  Some seem to be missing.
+		  	$scope.frameTen = function(a,b,c){
+			var i = 9;
+			$scope.toggle();
+			if (a == 10 && $scope.scores[i-2] == 'X' && $scope.scores[i-1] == 'X'){
+				$scope.scores[i-2] = 30;
+				$scope.scores[i-1]= 10 + (a+b);
+				$scope.scores.push(a+b+c);
+			} else if (a == 10 && $scope.scores[i-1] == 'X'){
+				$scope.scores[i-1] = 10 + (a+b);
+				$scope.scores.push(a+b+c);
+			} else if ($scope.scores[i-1] == '/'){
+				$scope.scores[i-1] = 10 + a;
+				$scope.scores.push(a+b+c);
+			} else {
+				$scope.scores.push(a+b+c);
+			}
+			$scope.total = $scope.scores.reduce(function (a,b) {
+		  		return a+b;
+		  	}, 0);
 		}
-		$scope.overallTotal = function(){
-			var total = parsefloat(0);
-			for (var j=0; j < $scope.scores.length; j++){
-				total += $scope.scores[i].total;
-			} return total;
-		}
+			$scope.toggle = function() {
+        		$scope.myVar = !$scope.myVar;
+    		};
 	};
 
 	$scope.newGame = function(){
 		$scope.scores=[]
+		frameNumber=0;
+		$scope.total = $scope.scores.reduce(function (a,b) {
+	  		return a+b;
+	  	}, 0);
+	  	//need to fix the toggle() to return the enter score button
+		//would like to add a clear input boxes functionality here
 	};
 
 }]);
